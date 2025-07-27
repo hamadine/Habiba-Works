ddc6f1db5ad17878b8dc16d5725167bf627e2878
hamadine <212258452+hamadine@users.noreply.github.com>
2025-07-27 20:50:02 +0100

    Update main.jsx

--- client/SRC/main.jsx
+++ client/SRC/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import AppRoutes from './routes'
+ReactDOM.createRoot(document.getElementById('root')).render(<AppRoutes />)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
